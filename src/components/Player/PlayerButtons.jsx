0faabe9bedf4841cd92dda4624a7c0233654f6a5
chronoDave <davidwolters6758@gmail.com>
2019-04-27 13:53:36 +0200

    Added volume knob

--- src/components/Player/PlayerButtons.jsx
+++ src/components/Player/PlayerButtons.jsx
@@ -9,13 +9,23 @@ import PlayIcon from '@material-ui/icons/PlayArrow';
 import PauseIcon from '@material-ui/icons/Pause';
 import NextIcon from '@material-ui/icons/SkipNext';
 import ShuffleIcon from '@material-ui/icons/Shuffle';
+import MuteIcon from '@material-ui/icons/VolumeOff';
+import VolumeLowIcon from '@material-ui/icons/VolumeMute';
+import VolumeMediumIcon from '@material-ui/icons/VolumeDown';
+import VolumeHighIcon from '@material-ui/icons/VolumeUp';
+
+// Experimental
+import Slider from '@material-ui/lab/Slider';
 
 // Core
 import withStyles from '@material-ui/core/styles/withStyles';
 import IconButton from '@material-ui/core/IconButton';
 
+import Typography from '../Typography/Typography';
+import Divider from '../Divider/Divider';
+
 // Actions
-import { setStatus, setPosition } from '../../actions/songActions';
+import { setStatus, setPosition, setVolume } from '../../actions/songActions';
 import { setIndex, shufflePlaylist } from '../../actions/playlistActions';
 
 // Utils
@@ -23,7 +33,7 @@ import { cleanUrl } from '../../functions';
 
 // Style
 import PlayerButtonsStyle from './PlayerButtonsStyle';
-import { GridContainer } from '../Grid';
+import { GridContainer, GridItem } from '../Grid';
 
 const PlayerButtons = props => {
   const {
@@ -37,7 +47,8 @@ const PlayerButtons = props => {
     playlistIndex,
     playlistSize,
     setPlaylistIndex,
-    shuffle
+    shuffle,
+    setSystemVolume
   } = props;
 
   const handleNext = () => {
@@ -58,39 +69,89 @@ const PlayerButtons = props => {
     setPlaylistIndex(0);
   };
 
+  const handleScroll = delta => {
+    const newVolume = volume + (delta / 10);
+
+    if (newVolume <= 100 && newVolume >= 0) setSystemVolume(newVolume);
+  };
+
+  const getVolumeIcon = () => {
+    if (volume === 0) return <MuteIcon fontSize="small" classes={{ root: classes.icon }} />;
+    if (volume <= 33) return <VolumeLowIcon fontSize="small" classes={{ root: classes.icon }} />;
+    if (volume <= 66) return <VolumeMediumIcon fontSize="small" classes={{ root: classes.icon }} />;
+    return <VolumeHighIcon fontSize="small" classes={{ root: classes.icon }} />;
+  };
+
   return (
     <Fragment>
       <GridContainer
-        wrap="nowrap"
-        justify="space-around"
+        direction="column"
+        alignItems="center"
       >
-        <IconButton
-          classes={{ root: classes.root }}
-          onClick={() => handlePrevious()}
+        <GridContainer
+          wrap="nowrap"
+          justify="space-around"
+          classes={{ root: classes.container }}
         >
-          <PreviousIcon color="inherit" />
-        </IconButton>
-        <IconButton
-          classes={{ root: classes.root }}
-          onClick={() => setSongStatus(status === 'PLAYING' ? 'PAUSED' : 'PLAYING')}
+          <IconButton
+            classes={{ root: classes.root }}
+            onClick={() => handlePrevious()}
+          >
+            <PreviousIcon color="inherit" />
+          </IconButton>
+          <IconButton
+            classes={{ root: classes.root }}
+            onClick={() => setSongStatus(status === 'PLAYING' ? 'PAUSED' : 'PLAYING')}
+          >
+            {status === 'PLAYING'
+              ? <PauseIcon color="inherit" />
+              : <PlayIcon color="inherit" />
+            }
+          </IconButton>
+          <IconButton
+            classes={{ root: classes.root }}
+            onClick={() => handleNext()}
+          >
+            <NextIcon color="inherit" />
+          </IconButton>
+          <IconButton
+            classes={{ root: classes.root }}
+            onClick={() => handleShuffle()}
+          >
+            <ShuffleIcon color="inherit" />
+          </IconButton>
+        </GridContainer>
+        <Divider light classes={{ root: classes.divider }} />
+        <GridContainer
+          wrap="nowrap"
+          justify="space-between"
+          alignItems="center"
+          classes={{ root: classes.container }}
         >
-          {status === 'PLAYING'
-            ? <PauseIcon color="inherit" />
-            : <PlayIcon color="inherit" />
-          }
-        </IconButton>
-        <IconButton
-          classes={{ root: classes.root }}
-          onClick={() => handleNext()}
-        >
-          <NextIcon color="inherit" />
-        </IconButton>
-        <IconButton
-          classes={{ root: classes.root }}
-          onClick={() => handleShuffle()}
-        >
-          <ShuffleIcon color="inherit" />
-        </IconButton>
+          <GridItem xs={3}>
+            <IconButton
+              onClick={() => setSystemVolume(0)}
+              onWheel={event => handleScroll(event.deltaY)}
+            >
+              {getVolumeIcon()}
+            </IconButton>
+          </GridItem>
+          <GridItem xs={1}>
+            <Typography variant="caption">
+              {Math.floor(volume)}
+            </Typography>
+          </GridItem>
+          <GridItem xs={8} classes={{ root: classes.slider }}>
+            <Slider
+              value={volume}
+              onChange={(event, value) => setSystemVolume(value)}
+              classes={{
+                track: classes.track,
+                thumb: classes.thumb
+              }}
+            />
+          </GridItem>
+        </GridContainer>
       </GridContainer>
       <Sound
         url={cleanUrl(url)}
@@ -115,7 +176,8 @@ PlayerButtons.propTypes = {
   playlistIndex: PropTypes.number,
   playlistSize: PropTypes.number,
   setPlaylistIndex: PropTypes.func,
-  shuffle: PropTypes.func
+  shuffle: PropTypes.func,
+  setSystemVolume: PropTypes.func
 };
 
 const mapStateToProps = state => ({
@@ -129,6 +191,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   setSongStatus: status => dispatch(setStatus(status)),
+  setSystemVolume: volume => dispatch(setVolume(volume)),
   setSongPosition: position => dispatch(setPosition(position)),
   setPlaylistIndex: index => dispatch(setIndex(index)),
   shuffle: () => dispatch(shufflePlaylist())
--- src/components/Player/PlayerButtonsStyle.jsx
+++ src/components/Player/PlayerButtonsStyle.jsx
@@ -2,6 +2,25 @@ const PlayerButtonsStyle = theme => ({
   root: {
     color: theme.palette.grey[50],
     padding: 0
+  },
+  track: {
+    height: 2
+  },
+  thumb: {
+    width: 6,
+    height: 6
+  },
+  icon: {
+    color: theme.palette.grey[50]
+  },
+  slider: {
+    padding: `0 ${theme.spacing.unit * 2}px`
+  },
+  container: {
+    height: 30
+  },
+  divider: {
+    margin: `${theme.spacing.unit * 2}px 0`
   }
 });
 
