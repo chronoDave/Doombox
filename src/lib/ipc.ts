f9d4c32477e5b36f3dcc754b028fa6c2b230a2f7
David Wolters <david.wolters@OJ.LOCAL>
2021-07-23 16:40:16 +0200

    Removed Enum

--- src/lib/ipc.ts
+++ src/lib/ipc.ts
@@ -1,26 +1,24 @@
-export type IpcAction = keyof typeof IpcActions;
-export enum IpcActions {
-  'GET' = 'GET',
-  'SET' = 'SET',
-  'ERROR' = 'ERROR',
-  'INSERT' = 'INSERT',
-  'FIND' = 'FIND',
-  'FIND_BY_ID' = 'FIND_BY_ID',
-  'UPDATE' = 'UPDATE',
-  'UPDATE_BY_ID' = 'UPDATE_BY_ID',
-  'DELETE' = 'DELETE',
-  'DELETE_BY_ID' = 'DELETE_BY_ID',
-  'DROP' = 'DROP',
-  'MINIMIZE' = 'MINIMIZE',
-  'MAXIMIZE' = 'MAXIMIZE',
-  'CLOSE' = 'CLOSE'
-}
+export const IPC_ACTIONS = [
+  'GET',
+  'SET',
+  'ERROR',
+  'INSERT',
+  'FIND',
+  'FIND_BY_ID',
+  'UPDATE',
+  'UPDATE_BY_ID',
+  'DELETE',
+  'DELETE_BY_ID',
+  'DROP',
+  'MINIMIZE',
+  'MAXIMIZE',
+  'CLOSE',
+] as const;
+export type IpcAction = typeof IPC_ACTIONS[number];
 
-export type IpcChannel = keyof typeof IpcChannels;
-export enum IpcChannels {
-  'WINDOW' = 'WINDOW',
-  'THEME' = 'THEME'
-}
+export type IpcChannel =
+  'WINDOW' |
+  'THEME';
 
 export type IpcController<T = unknown> = Partial<Record<IpcAction, (payload: IpcPayload) => Promise<T>>>;
 export type IpcPayload<T = unknown> = {
@@ -35,7 +33,7 @@ export default class Ipc {
   }
 
   private static isPayload(x: Partial<IpcPayload>): x is IpcPayload {
-    return !!x.action && x.action in IpcActions;
+    return !!x.action && IPC_ACTIONS.includes(x.action);
   }
 
   static validate<T = unknown>(x: unknown): x is IpcPayload<T> {
