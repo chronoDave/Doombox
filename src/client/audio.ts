6362202739d10d9bd74f087d6fede880d11e3e41
David Wolters <david.wolters@OJ.LOCAL>
2021-08-02 13:44:01 +0200

    Added volume properties

--- src/client/audio.ts
+++ src/client/audio.ts
@@ -1,11 +1,30 @@
 import fs from 'fs';
+import EventEmitter from 'events';
 
 import { ipcError } from './ipc/ipc';
 
-export default class {
-  private static context = new window.AudioContext();
+const AUDIO_EVENTS = {
+  MUTE: 'MUTE',
+  VOLUME: 'VOLUME'
+} as const;
 
-  static async load(src: string) {
+export type AudioEvent = keyof typeof AUDIO_EVENTS;
+
+export default new class extends EventEmitter {
+  private context = new window.AudioContext();
+  private nodeGain: GainNode;
+
+  private _volume = 0;
+  private _muted = false;
+
+  constructor() {
+    super();
+
+    this.nodeGain = this.context.createGain();
+    this.nodeGain.connect(this.context.destination);
+  }
+
+  async load(src: string) {
     try {
       const file = await fs.promises.readFile(src);
       const buffer = await this.context.decodeAudioData(file.buffer);
@@ -22,4 +41,26 @@ export default class {
       return Promise.reject(err);
     }
   }
-}
+
+  mute(muted: boolean) {
+    this._muted = muted;
+
+    if (this._muted) {
+      this.nodeGain.gain.setValueAtTime(0, this.context.currentTime);
+    } else {
+      this.nodeGain.gain.setValueAtTime(this.volume, this.context.currentTime);
+    }
+
+    this.emit(AUDIO_EVENTS.MUTE, this._muted);
+  }
+
+  get volume() {
+    return this._volume;
+  }
+
+  set volume(volume: number) {
+    this._volume = volume;
+    this.nodeGain.gain.setValueAtTime(0, this.context.currentTime);
+    this.emit(AUDIO_EVENTS.VOLUME, this._volume);
+  }
+}();
