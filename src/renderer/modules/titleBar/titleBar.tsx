0b7372923ff91e0e28744c6e1c84ad902f66c493
chronoDave <davidwolters6758@gmail.com>
2022-08-07 00:58:38 +0200

    Added fonts & image theme switch

--- src/renderer/modules/titleBar/titleBar.tsx
+++ src/renderer/modules/titleBar/titleBar.tsx
@@ -8,39 +8,58 @@ import './titleBar.scss';
 
 export type TitleBarProps = {};
 
-const TitleBar: Component<TitleBarProps> = () => new forgo.Component({
-  render() {
-    return (
-      <header class='TitleBar'>
-        <img class='light' src='icons/icon_light.png' alt='Doombox app icon' />
-        <img class='dark' src='icons/icon_dark.png' alt='Doombox app icon' />
-        <h1>Doombox</h1>
-        <nav>
-          <button
-            type='button'
-            aria-label='minimize'
-            onclick={window.ipc.minimize}
-          >
-            <Icon id='minimize' />
-          </button>
-          <button
-            type='button'
-            aria-label='maximize'
-            onclick={window.ipc.maximize}
-          >
-            <Icon id='maximize' />
-          </button>
-          <button
-            type='button'
-            aria-label='close'
-            onclick={window.ipc.close}
-          >
-            <Icon id='close' />
-          </button>
-        </nav>
-      </header>
-    );
-  }
-});
+const TitleBar: Component<TitleBarProps> = () => {
+  const query = window.matchMedia('(prefers-color-scheme: dark)');
+  let variant: 'light' | 'dark' = query.matches ? 'light' : 'dark';
+
+  const component = new forgo.Component({
+    render() {
+      return (
+        <header class='TitleBar'>
+          <img src={`icons/icon_${variant}.png`} alt='Doombox app icon' />
+          <h1 class='font-normal'>Doombox</h1>
+          <nav>
+            <button
+              type='button'
+              aria-label='minimize'
+              onclick={window.ipc.minimize}
+            >
+              <Icon id='minimize' />
+            </button>
+            <button
+              type='button'
+              aria-label='maximize'
+              onclick={window.ipc.maximize}
+            >
+              <Icon id='maximize' />
+            </button>
+            <button
+              type='button'
+              aria-label='close'
+              onclick={window.ipc.close}
+            >
+              <Icon id='close' />
+            </button>
+          </nav>
+        </header>
+      );
+    }
+  });
+
+  const handleQuery = (mql: MediaQueryList | MediaQueryListEvent) => {
+    variant = mql.matches ? 'light' : 'dark';
+    component.update();
+  };
+
+  component.mount(() => {
+    query.addEventListener('change', handleQuery, { passive: true });
+  });
+
+  component.unmount(() => {
+    query.removeEventListener('change', handleQuery);
+  });
+
+  return component;
+};
 
 export default TitleBar;
--- src/renderer/preload.ts
+++ src/renderer/preload.ts
@@ -1,4 +1,6 @@
+import './scss/fonts.scss';
 import './scss/mixins.scss';
 import './scss/reset.scss';
 import './scss/theme.scss';
+import './scss/typography.scss';
 import './scss/core.scss';
