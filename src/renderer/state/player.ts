9d652bb6390b9d5873c0f425408040c1199b6cd6
chronoDave <davidwolters6758@gmail.com>
2023-04-23 17:56:52 +0100

    Removed circular dep

--- src/renderer/state/player.ts
+++ src/renderer/state/player.ts
@@ -2,27 +2,16 @@ import produce from 'immer';
 
 import appShape from '../../types/shapes/app.shape';
 import userShape from '../../types/shapes/user.shape';
-import Player, { PlayerStatus } from '../lib/player';
+import Player from '../lib/player';
 
-// eslint-disable-next-line import/no-cycle
-import { next } from './actions/playlist.actions';
 import store from './store';
 
 const player = new Player({
   ...appShape.player,
   ...userShape.player,
-  onstatus: status => {
-    if (
-      status === PlayerStatus.Ended &&
-      store.get().playlist.index < store.get().playlist.songs.length
-    ) {
-      next();
-    } else {
-      store.dispatch(produce(draft => {
-        draft.player.status = status;
-      }), 'player.status');
-    }
-  },
+  onstatus: status => store.dispatch(produce(draft => {
+    draft.player.status = status;
+  }), 'player.status'),
   onduration: duration => store.dispatch(produce(draft => {
     draft.player.current.duration = duration;
   }), 'player.duration'),
--- src/renderer/state/store.ts
+++ src/renderer/state/store.ts
@@ -1,7 +1,44 @@
+import type { State } from '../types/state';
+
+import themeShape from '../../types/shapes/theme.shape';
+import userShape from '../../types/shapes/user.shape';
+import { PlayerStatus } from '../lib/player';
 import Store from '../lib/store';
+import { AppView, SettingsView } from '../types/view';
 
-import state from './state';
-
-const store = new Store(state);
-
-export default store;
+export default new Store<State>({
+  app: {
+    ready: false,
+    scanning: false
+  },
+  player: {
+    muted: false,
+    volume: 100,
+    status: PlayerStatus.Stopped,
+    current: {
+      id: '',
+      duration: 0,
+      position: 0
+    }
+  },
+  playlist: {
+    index: 0,
+    songs: []
+  },
+  view: {
+    app: AppView.Label,
+    settings: SettingsView.Library
+  },
+  search: {
+    songs: null,
+    albums: null,
+    labels: null
+  },
+  entities: {
+    song: new Map(),
+    album: new Map(),
+    label: new Map()
+  },
+  theme: themeShape,
+  user: userShape
+});
