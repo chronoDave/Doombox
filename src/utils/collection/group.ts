4494d1364b033660dad3d08eb1d45dcd93c8b12d
chronoDave <davidwolters6758@gmail.com>
2023-04-28 22:33:55 +0100

    Fixed group type error

--- src/utils/collection/group.ts
+++ src/utils/collection/group.ts
@@ -1,4 +1,4 @@
-export default <T extends Record<PropertyKey, keyof any>>(arr: T[], key: keyof T) => arr
+export default <T extends Record<PropertyKey, any>>(arr: T[], key: keyof T) => arr
   .reduce<Record<PropertyKey, T[]>>((acc, cur) => {
     (acc[cur[key]] ||= []).push(cur);
     return acc;
