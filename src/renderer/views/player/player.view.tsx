96d15566ef6e984a6f552d50e512669facf6fbfc
chronoDave <davidwolters6758@gmail.com>
2023-05-27 01:29:12 +0100

    Refactored player view

--- src/renderer/views/player/player.view.tsx
+++ src/renderer/views/player/player.view.tsx
@@ -2,18 +2,10 @@ import type { ForgoNewComponentCtor as Component } from 'forgo';
 
 import * as forgo from 'forgo';
 
-import secToTime from '../../../utils/time/secToTime';
-import timeToHhMmSs from '../../../utils/time/timeToHhMmSs';
 import Icon from '../../components/icon/icon';
-import VirtualList from '../../components/virtualList/virtualList';
 import { AudioStatus } from '../../lib/audio';
-import * as player from '../../state/actions/player.actions';
 import { next, pause, previous } from '../../state/actions/player.actions';
 import { playerStatusSelector } from '../../state/selectors/player.selectors';
-import { queueIndexSelector, queueSelector } from '../../state/selectors/queue.selectors';
-import { songSelector } from '../../state/selectors/song.selectors';
-import { romajiSelector } from '../../state/selectors/user.selectors';
-import cx from '../../utils/cx/cx';
 
 import PlayerCover from './playerCover/playerCover';
 import PlayerMetadata from './playerMetadata/playerMetadata';
@@ -27,16 +19,17 @@ export type PlayerViewProps = {};
 const PlayerView: Component<PlayerViewProps> = () => {
   const component = new forgo.Component<PlayerViewProps>({
     render() {
-      const queue = queueSelector.get();
       const playerStatus = playerStatusSelector.get();
 
       return (
         <div class='View PlayerView'>
-          <div class='panel meta'>
+          <div class='metadata'>
             <PlayerCover />
             <PlayerMetadata />
+          </div>
+          <div class='controls'>
             <PlayerSlider />
-            <div class='PlayerControls'>
+            <div class='buttons'>
               <PlayerVolume />
               <button type='button' onclick={() => previous()}>
                 <Icon id='previous' />
@@ -49,41 +42,11 @@ const PlayerView: Component<PlayerViewProps> = () => {
               </button>
             </div>
           </div>
-          <VirtualList
-            list={queue}
-            item={{
-              height: 38,
-              render: (id, i) => {
-                const song = songSelector.get(id);
-
-                return (
-                  <button
-                    id={song._id}
-                    type='button'
-                    class={cx(queueIndexSelector.get() === i && 'active')}
-                    onclick={() => player.skip(i)}
-                  >
-                    <div class='metadata'>
-                      <p>{romajiSelector.get(song.title)}</p>
-                      <p>{romajiSelector.get(song.artist)}</p>
-                    </div>
-                    <div class='duration'>
-                      <p>{timeToHhMmSs(secToTime(song.duration ?? 0))}</p>
-                    </div>
-                  </button>
-                );
-              }
-            }}
-          />
         </div>
       );
     }
   });
 
-  queueSelector.subscribe(component);
-  songSelector.subscribe(component);
-  queueIndexSelector.subscribe(component);
-  romajiSelector.subscribe(component);
   playerStatusSelector.subscribe(component);
 
   return component;
--- src/renderer/views/player/playerMetadata/playerMetadata.tsx
+++ src/renderer/views/player/playerMetadata/playerMetadata.tsx
@@ -2,7 +2,9 @@ import type { ForgoNewComponentCtor as Component } from 'forgo';
 
 import * as forgo from 'forgo';
 
+import Icon from '../../../components/icon/icon';
 import { playerSongSelector } from '../../../state/selectors/player.selectors';
+import { queueIndexSelector, queueSelector } from '../../../state/selectors/queue.selectors';
 
 import './playerMetadata.scss';
 
@@ -12,18 +14,30 @@ const PlayerMetadata: Component<PlayerMetadataProps> = () => {
   const component = new forgo.Component<PlayerMetadataProps>({
     render() {
       const current = playerSongSelector.get();
+      const queue = queueSelector.get();
+      const queueIndex = queueIndexSelector.get();
 
       return (
-        <div class='PlayerMeta'>
-          <span class='scroll title'>{current?.title ?? 'unknown'}</span>
-          <span class='scroll artist'>{current?.artist ?? 'unknown'}</span>
-          <span class='scroll label'>{current?.label ?? 'unknown'}</span>
+        <div class='PlayerMetadata'>
+          <span class='title'>{current?.title ?? 'unknown'}</span>
+          <span class='artist'>{current?.artist ?? 'unknown'}</span>
+          <span class='album'>{current?.album ?? 'unknown'}</span>
+          {queue.length > 0 ? (
+            <span class='queue'>
+              <Icon id='playlistMusic' />
+              {queueIndex + 1} / {queue.length}
+            </span>
+          ) : null}
         </div>
       );
     }
   });
 
-  return playerSongSelector.subscribe(component);
+  playerSongSelector.subscribe(component);
+  queueSelector.subscribe(component);
+  queueIndexSelector.subscribe(component);
+
+  return component;
 };
 
 export default PlayerMetadata;
