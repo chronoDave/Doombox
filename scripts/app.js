90e36881f5efaf257d6010928dd10fa03bcaad52
chronoDave <davidwolters6758@gmail.com>
2023-07-31 22:51:09 +0100

    Simplified build script

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,14 @@
 const minimist = require('minimist');
 
 const esbuild = require('./esbuild/esbuild');
+const clean = require('./esbuild/plugins/clean');
 const copy = require('./esbuild/plugins/copy');
 const log = require('./esbuild/plugins/log');
+const sass = require('./sass/sass');
 
 const { w, dev } = minimist(process.argv.slice(2));
 
+// App
 esbuild({
   entryPoints: [
     { in: 'src/app/index.ts', out: 'app' },
@@ -36,3 +39,42 @@ esbuild({
     }])
   ]
 }, { watch: w });
+
+// Renderer
+esbuild({
+  entryPoints: ['src/renderer/index.tsx'],
+  define: {
+    'process.env.NODE_ENV': w ?
+      '"development"' :
+      '"production"',
+    'process.env.DOM': dev ?
+      '"development"' :
+      '"production"'
+  },
+  bundle: true,
+  outdir: 'build/app/renderer',
+  outbase: 'src/renderer',
+  plugins: [
+    log('renderer'),
+    clean(['build/renderer']),
+    copy([{
+      in: 'src/renderer/index.html',
+      out: 'build/app/renderer/index.html'
+    }, {
+      in: 'src/renderer/assets/fonts',
+      out: 'build/app/renderer/fonts'
+    }, {
+      in: 'src/renderer/assets/icons',
+      out: 'build/app/renderer/icons'
+    }])]
+}, { watch: w });
+
+sass([{
+  in: 'src/renderer/index.scss',
+  out: 'build/app/renderer',
+  depedencies: [
+    'src/renderer/scss',
+    'src/renderer/modules',
+    'src/renderer/views'
+  ]
+}]);
