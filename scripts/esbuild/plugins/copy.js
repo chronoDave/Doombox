33d58796fc45f3878d91202fbfe6b08426618560
chronoDave <davidwolters6758@gmail.com>
2023-09-02 14:17:49 +0100

    Added initial copy

--- scripts/esbuild/plugins/copy.js
+++ scripts/esbuild/plugins/copy.js
@@ -6,20 +6,22 @@ module.exports = (dirs, options) => ({
   name: 'copy',
   setup: () => {
     const cwd = options?.cwd ?? process.cwd();
+    const copy = dir => {
+      console.time(`[copy] copied ${dir.in}`);
+      fs.rmSync(path.resolve(cwd, dir.out), { recursive: true, force: true });
+      fs.cpSync(path.resolve(cwd, dir.in), path.resolve(cwd, dir.out), { recursive: true });
+      console.timeEnd(`[copy] copied ${dir.in}`);
+    };
+
+    dirs.forEach(copy);
 
     chokidar.watch(dirs.map(dir => dir.in), {
       cwd,
       awaitWriteFinish: true
     })
       .on('change', file => {
-        console.time(`[copy] copied ${file}`);
-
         const dir = dirs.find(x => file.split(path.sep).join(path.posix.sep).includes(x.in));
-        if (dir) {
-          fs.rmSync(path.resolve(cwd, dir.out), { recursive: true, force: true });
-          fs.cpSync(path.resolve(cwd, dir.in), path.resolve(cwd, dir.out), { recursive: true });
-          console.timeEnd(`[copy] copied ${file}`);
-        }
+        if (dir) copy(dir);
       });
   }
 });
