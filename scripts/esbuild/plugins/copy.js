6da3c8b5299debe02fb6ef6d84325fd588a519c2
chronoDave <davidwolters6758@gmail.com>
2023-09-02 14:15:39 +0100

    Updated copy plugin

--- scripts/esbuild/plugins/copy.js
+++ scripts/esbuild/plugins/copy.js
@@ -1,18 +1,25 @@
+const chokidar = require('chokidar');
 const fs = require('fs');
 const path = require('path');
 
-module.exports = (folders, options) => ({
+module.exports = (dirs, options) => ({
   name: 'copy',
-  setup: build => {
-    build.onEnd(() => {
-      folders.forEach(folder => {
-        const cwd = options?.cwd ?? process.cwd();
-        const dirIn = path.resolve(cwd, folder.in);
-        const dirOut = path.resolve(cwd, folder.out);
+  setup: () => {
+    const cwd = options?.cwd ?? process.cwd();
 
-        fs.rmSync(dirOut, { recursive: true, force: true });
-        fs.cpSync(dirIn, dirOut, { recursive: true });
+    chokidar.watch(dirs.map(dir => dir.in), {
+      cwd,
+      awaitWriteFinish: true
+    })
+      .on('change', file => {
+        console.time(`[copy] copied ${file}`);
+
+        const dir = dirs.find(x => file.split(path.sep).join(path.posix.sep).includes(x.in));
+        if (dir) {
+          fs.rmSync(path.resolve(cwd, dir.out), { recursive: true, force: true });
+          fs.cpSync(path.resolve(cwd, dir.in), path.resolve(cwd, dir.out), { recursive: true });
+          console.timeEnd(`[copy] copied ${file}`);
+        }
       });
-    });
   }
 });
