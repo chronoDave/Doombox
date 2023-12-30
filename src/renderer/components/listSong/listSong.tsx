2dda0b5a8c8715340f0e429bbc186420946d549d
David Wolters <davidwolters6758@gmail.com>
2023-12-30 09:31:53 +0000

    Improved listSong UI

--- src/renderer/components/listSong/listSong.tsx
+++ src/renderer/components/listSong/listSong.tsx
@@ -4,6 +4,7 @@ import type { ForgoNewComponentCtor as Component } from 'forgo';
 import * as forgo from 'forgo';
 
 import { play } from '../../actions/player.actions';
+import Glyph from '../glyph/glyph';
 import VirtualList from '../virtualList/virtualList';
 
 import './listSong.scss';
@@ -21,8 +22,18 @@ const ListSong: Component<ListSongProps> = () => {
           onclick={(action, id) => play(id)}
           cell={{
             id: cell => cell._id,
-            height: () => 48,
-            render: cell => <div class='ListSong'>{cell.title}</div>
+            height: () => 52,
+            render: cell => (
+              <button
+                class='ListSong'
+                type='button'
+                data-id={cell._id}
+                data-action='song-play'
+              >
+                <p class='title nowrap'>{cell.title}</p>
+                <p class='subtitle nowrap'>{cell.artist}<Glyph id='dot' />{cell.album}</p>
+              </button>
+            )
           }}
         />
       );
