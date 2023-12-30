c10eb76196dc70345115e759b88ea24d1dff30dc
David Wolters <davidwolters6758@gmail.com>
2023-12-30 09:37:39 +0000

    Added librarySearch active state

--- src/renderer/ui/library/librarySearch/librarySearch.state.ts
+++ src/renderer/ui/library/librarySearch/librarySearch.state.ts
@@ -1,4 +1,7 @@
 import store from '../../../store';
 import createSelector from '../../../utils/createSelector';
 
-export default createSelector(store, 'library')(state => state.search.query);
+export default createSelector(store, 'library')(state => ({
+  query: state.search.query,
+  route: state.route.search
+}));
--- src/renderer/ui/library/librarySearch/librarySearch.tsx
+++ src/renderer/ui/library/librarySearch/librarySearch.tsx
@@ -7,45 +7,53 @@ import { setRouteSearch } from '../../../actions/route.actions';
 import Icon from '../../../components/icon/icon';
 import InputSearch from '../../../components/inputSearch/inputSearch';
 import * as Route from '../../../types/route';
+import cx from '../../../utils/cx/cx';
 
 import subscribe from './librarySearch.state';
+
+import './librarySearch.scss';
 
 export type LibrarySearchProps = {};
 
 const LibrarySearch: Component<LibrarySearchProps> = () => {
   const component = new forgo.Component<LibrarySearchProps>({
     render() {
-      const query = subscribe(component);
+      const state = subscribe(component);
 
-      return [
-        <div class='bar'>
-          <InputSearch placeholder='Search...' onsubmit={search} />
-        </div>,
-        query && (
-          <nav class='bar' aria-label='Search'>
-            <button
-              type='button'
-              onclick={() => setRouteSearch(Route.Search.Label)}
-              aria-label='Label'
-            >
-              <Icon id='record' />
-            </button>
-            <button
-              type='button'
-              onclick={() => setRouteSearch(Route.Search.Album)}
-              aria-label='Album'
-            >
-              <Icon id='boxMusic' />
-            </button>
-            <button
-              type='button'
-              onclick={() => setRouteSearch(Route.Search.Song)}
-              aria-label='Song'
-            >
-              <Icon id='musicNote' />
-            </button>
-          </nav>
-        )];
+      return (
+        <div class='LibrarySearch'>
+          <div class='bar'>
+            <InputSearch placeholder='Search...' onsubmit={search} />
+          </div>
+          {state.query && (
+            <nav class='bar' aria-label='Search'>
+              <button
+                type='button'
+                onclick={() => setRouteSearch(Route.Search.Label)}
+                aria-label='Label'
+                class={cx(state.route === Route.Search.Label && 'active')}
+              >
+                <Icon id='record' />
+              </button>
+              <button
+                type='button'
+                onclick={() => setRouteSearch(Route.Search.Album)}
+                aria-label='Album'
+                class={cx(state.route === Route.Search.Album && 'active')}
+              >
+                <Icon id='boxMusic' />
+              </button>
+              <button
+                type='button'
+                onclick={() => setRouteSearch(Route.Search.Song)}
+                aria-label='Song'
+                class={cx(state.route === Route.Search.Song && 'active')}
+              >
+                <Icon id='musicNote' />
+              </button>
+            </nav>)}
+        </div>
+      );
     }
   });
 
