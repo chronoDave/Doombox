e2952bb5802fd48227fb6a98f32ddd4c84e0c77f
chronoDave <davidwolters6758@gmail.com>
2024-04-15 21:06:54 +0100

    Fixed clean firing every change

--- scripts/esbuild/plugins/clean.js
+++ scripts/esbuild/plugins/clean.js
@@ -3,17 +3,15 @@ const path = require('path');
 
 module.exports = dirs => ({
   name: 'clean',
-  setup: build => {
-    build.onStart(() => {
-      dirs.forEach(dir => {
-        const folder = path.isAbsolute(dir) ?
-          dir :
-          path.join(process.cwd(), dir);
+  setup: () => {
+    dirs.forEach(dir => {
+      const folder = path.isAbsolute(dir) ?
+        dir :
+        path.join(process.cwd(), dir);
 
-        fs.rmSync(folder, {
-          recursive: true,
-          force: true
-        });
+      fs.rmSync(folder, {
+        recursive: true,
+        force: true
       });
     });
   }
